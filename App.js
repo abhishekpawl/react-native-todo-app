fdb86588252589bf13551727879c93a6e5a1fb77
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-18 21:22:25 +0530

    Added basic layout

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LinearGradient colors={['#ebbba7', '#cfc7f8']} style={styles.container}>
+        <Text style={styles.content}>App</Text>
+      </LinearGradient>
     </View>
   );
 }
@@ -15,6 +16,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    color: 'black',
   },
+  content: {
+    padding: 40,
+    color: 'black'
+  }
 });
